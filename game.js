036da28bb38e744d3ef7f7931b32b54039065a30
zahra <z.ouikene@gmail.com>
2023-03-15 13:48:39 +0100

    DICE

---
+++ game.js
@@ -0,0 +1,18 @@
+let palyerOne;
+let palyerTowe;
+const lancerDe = function(){
+    const nombreDecimal=(Math.random()*6)+1;
+    const nombre= Math.trune(nombreDecimal);
+    return nombre;
+}
+const deTexte = {
+    '1': 'Un',
+    '2': 'Deux',
+    '3': 'Trois',
+    '4': 'Quatre',
+    '5': 'Cinq',
+    '6': 'Six',
+}
+console.log(deText['1']);
+const resultat= lancerDe();
+console.log(lancerDe);
