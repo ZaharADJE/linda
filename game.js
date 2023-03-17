16ab266833bd66caaa5adb27c6059aa41217e321
zahra <z.ouikene@gmail.com>
2023-03-17 11:49:56 +0100

    DICE

--- game.js
+++ game.js
@@ -1,8 +1,15 @@
-let palyerOne;
-let palyerTowe;
-const lancerDe = function(){
+const playerOne= document.getElementById('playerOne');
+const playerTowe= document.getElementById('playerTowe');
+const BoutonLancer=document.getElementById('boutonLancer');
+const image= document.getElementById('image_dé');
+const ScoreOne= document.getElementById('ScoreOne');
+const ScoreTowe= document.getElementById('ScoreTowe');
+
+
+
+function lancerDe(){
     const nombreDecimal=(Math.random()*6)+1;
-    const nombre= Math.trune(nombreDecimal);
+    const nombre= Math.floor(nombreDecimal);
     return nombre;
 }
 const deTexte = {
@@ -13,6 +20,26 @@ const deTexte = {
     '5': 'Cinq',
     '6': 'Six',
 }
-console.log(deText['1']);
+console.log(deTexte['1']);
+console.log(deTexte['2']);
+console.log(deTexte['3']);
+console.log(deTexte['4']);
+console.log(deTexte['5']);
+console.log(deTexte['6']);
 const resultat= lancerDe();
-console.log(lancerDe);
+console.log(resultat+'('+deTexte[resultat]+')');
+
+function clickSurBouton(){
+    const resultat=lancerDe();
+    image.src='image/Dé de '+resultat+'.png';
+} 
+BoutonLancer.addEventListener('click', e =>{
+
+ if (playerOne.value === "" || playerTowe.value === ""){
+    alert("il y a pas le nom de player")
+ }else{
+    clickSurBouton();
+ }
+
+
+});
